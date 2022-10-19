5a0e569dbfb73407268f31891f850f3faad992d3
KakeruYamamoto <54306857+KakeruYamamoto@users.noreply.github.com>
2022-10-19 21:36:30 +0900

    Create: genderMapの作成

--- src/main/java/com/example/application/service/UserApplicationService.java
+++ src/main/java/com/example/application/service/UserApplicationService.java
@@ -1,15 +1,21 @@
 package com.example.application.service;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class UserApplicationService {
 
 	//** 性別のMapを生成す */
-	@GetMapping("/login")
-	public String getLogin() {
-		return "login/login";
+	public Map<String, Integer> getGenderMap() {
+
+		Map<String, Integer> genderMap = new LinkedHashMap<>();
+		genderMap.put("男性", 1);
+		genderMap.put("女性", 2);
+
+		return genderMap;
 	}
 
 }
