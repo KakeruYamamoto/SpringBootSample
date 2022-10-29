0074bd498ddbc49605cc4d862844c706c9e10a6b
KakeruYamamoto <54306857+KakeruYamamoto@users.noreply.github.com>
2022-10-29 19:15:10 +0900

    Add: コメント追加

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -13,16 +13,23 @@ public class HelloController {
 	@Autowired
 	private HelloService service;
 
+	// GetMappingはGetメソッドでHttpリクエストを受付られるようになる
 	@GetMapping("/hello")
 	public String getHello() {
 		//hello.htmlに画面遷移
 		return "hello";
 	}
 
+	//PostMappingで、Viewからのリクエスト（postメソッド)を受け付けられる
+	//Viewでは、formの入力：<input type="text" name="text1"  />のように、name="text1"で定義されている
+	//RequestParamは、上記で受け取ったtext1の値を引数の「String str」に代入してくれる
+	//注意：@RequestParamは、Htmlのname属性と名前をあわせること	
 	@PostMapping("/hello")
 	public String postRequest(@RequestParam("text1") String str, Model moedel) {
 
 		///画面から受け取った文字列をModelに登録
+		//ここで定義してるのは、（キー、バリュー）である。そのため、レスポンスをかえすViewでキー（sample）を指定するとその値が返される
+		//model.addAttributeメソッドで画面に渡したいデータをModelオブジェクトに追加
 		moedel.addAttribute("sample", str);
 
 		///response.htmlに画面遷移
