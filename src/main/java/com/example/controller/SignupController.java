3eeeac92d31d42d063da491ac80deaef6d130496
KakeruYamamoto <54306857+KakeruYamamoto@users.noreply.github.com>
2022-11-06 15:41:56 +0900

    Add : バリデーションの実行順序の設定を追加

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -47,7 +48,7 @@ public class SignupController {
 	 * 
 	 * */
 	@PostMapping("/signup")
-	public String postSignup(Model model, Locale locale, @ModelAttribute @Validated SignupForm form,
+	public String postSignup(Model model, Locale locale, @ModelAttribute @Validated(GroupOrder.class) SignupForm form,
 			BindingResult bindingResult) {
 
 		//入力チェック結果
